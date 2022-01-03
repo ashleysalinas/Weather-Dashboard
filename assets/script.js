90426be5cfdc508dae31a1ebafacbf8eb46b5ad7
ashleysalinas <ashleysalinas98@protonmail.com>
2022-01-02 21:39:18 -0600

    got the current day section working

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,52 @@
 var apiKey = "3aec6929e5ae8b8999152eb3d38997f4";
 var searchForm = document.querySelector("#search-form");
 var submitButton = document.querySelector("#submit-button")
-
+var currentCitySlot = document.querySelector("#current-city-slot")
+var currentCitySection = document.querySelector(".currentCity")
+var fiveDayForecastSection = document.querySelector(".fiveDayForecast")
 
 submitButton.addEventListener("click", handleCitySearch)
 
 function handleCitySearch(event) {
     event.preventDefault();
     var searchInputVal = document.querySelector("#searchInput").value.trim();
-    var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchInputVal + "&appid=" + apiKey;
+    var fiveDayRequestUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchInputVal + "&appid=" + apiKey; // 5 day forecase
+    var currentDayRequestUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInputVal + "&appid=" + apiKey + "&units=imperial";
 
-    fetch(requestUrl)
+    fetch(currentDayRequestUrl)
     .then(function (response) {
         return response.json()
     })
     .then(function (data) {
+        currentCitySlot.textContent = searchInputVal
+
+        //current temp
+        var tempSlot = document.createElement("h4")
+        tempSlot.textContent = "Temperature: " + data.main.temp + "°F";
+        currentCitySection.append(tempSlot)
+        
+        //current humidity
+        var humidSlot = document.createElement("h4")
+        humidSlot.textContent = "Humidity: " + data.main.humidity + "%"
+        currentCitySection.append(humidSlot)
+        //wind speed
+
+        var windSpeed = document.createElement("h4")
+        windSpeed.textContent = "Wind Speed: " + data.wind.speed + "MPH"
+        currentCitySection.append(windSpeed)
+       
+    })
+    //five day forecast
+    fetch(fiveDayRequestUrl)
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function(data) {
         console.log(data)
+        for (var i = 0; i < 5; i++) {
+            var dateSlot = document.createElement("h4");
+            dateSlot.textContent = data.list[i].dt_text
+            fiveDayForecastSection.append(dateSlot)
+        }
     })
 }
